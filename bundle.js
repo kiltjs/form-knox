95a8a5fb788c5f299ccbb8dee37a0d6cc6f2d3c3
Jesús Germade <jesus@germade.es>
2017-11-15 19:02:11 +0100

    removing old files

--- bundle.js
+++
@@ -1,9 +0,0 @@
-
-var formKnox = require('./form-knox'),
-    input = require('./input'),
-    mask = require('./mask');
-
-formKnox.input = input;
-formKnox.mask = mask;
-
-module.exports = formKnox(formKnox, mask);
--- umd.js
+++
@@ -1,9 +0,0 @@
-/* global define */
-
-(function (factory) {
-  if (typeof module === 'object' && module.exports) module.exports = factory();
-  else if ( 'define' in window && window.define.amd ) define([], factory);
-  else window.formKnox = factory();
-})(function () {
-  return require('./bundle');
-});
