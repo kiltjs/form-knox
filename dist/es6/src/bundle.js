4832dc46f84c3b0670f48e8887a576b64b7441b1
Jesús Germade <jesus@germade.es>
2017-11-15 19:13:23 +0100

    removing dist folder from master

--- dist/es6/src/bundle.js
+++
@@ -1,9 +0,0 @@
-
-import formKnox from './form-knox';
-import input from './input';
-import mask from './mask';
-
-formKnox.input = input;
-formKnox.mask = mask;
-
-export default formKnox(formKnox, mask);
