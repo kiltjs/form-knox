998302629bbbc79f279cf25779cb7e8b78cc3613
Jesús Germade <jesus@germade.es>
2018-06-22 18:23:48 +0200

    v1.0.71

---
+++ dist/form-knox.js
@@ -0,0 +1,96 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _utils = require('./utils');
+
+function formParams(form) {
+  if (!(form instanceof Element) && form.length) form = form[0];
+
+  var data = {};
+  [].forEach.call(form.elements, function (el) {
+    if (el.name && !el.disabled) {
+      if (el.type === 'radio') {
+        if (el.checked) data[el.name] = el.value;
+      } else {
+        data[el.name] = el.value;
+      }
+    }
+  });
+  return data;
+}
+
+function runListeners(listeners, args, this_arg) {
+  for (var i = 0, n = listeners.length; i < n; i++) {
+    listeners[i].apply(this_arg, args);
+  }
+}
+
+function formBind(form, onSubmit, options) {
+  options = options || {};
+
+  if (!(onSubmit instanceof Function)) {
+    options = onSubmit || options;
+    onSubmit = _utils._noop;
+  }
+
+  if (options.novalidate) form.setAttribute('novalidate', '');
+
+  var listeners = { invalid: [], reset: [], submitting: [] },
+      use_capture = options.use_capture || options.use_capture === undefined,
+      _onSubmit = function _onSubmit(e) {
+
+    if (form.hasAttribute('novalidate')) {
+      var valid = form.checkValidity(),
+          input_invalid = form.querySelector(':invalid');
+
+      onSubmit(e, valid);
+      if (e.defaultPrevented) return;
+
+      if (!valid) runListeners(listeners.invalid, [], form);
+      runListeners(listeners.submitting, [valid], form);
+
+      if (options.focus_invalid && input_invalid) input_invalid.focus();
+    } else {
+      runListeners(listeners.submitting, [true], form);
+      onSubmit(e, true);
+    }
+  };
+
+  form.addEventListener('submit', _onSubmit, use_capture);
+  form.addEventListener('invalid', function _onInvalid() {
+    runListeners(listeners.invalid, [], form);
+    if (!form.hasAttribute('novalidate')) runListeners(listeners.submitting, [false], form);
+    form.removeEventListener('invalid', _onInvalid, use_capture);
+    setTimeout(function () {
+      form.addEventListener('invalid', _onInvalid, use_capture);
+    });
+  }, use_capture);
+
+  var instance = {
+    form: form,
+    on: function on(event_name, listener, use_capture) {
+      if (listeners[event_name]) listeners[event_name].push(listener);else form.addEventListener(event_name, listener, use_capture);
+      return instance;
+    },
+    off: function off(event_name, listener, use_capture) {
+      if (listeners[event_name]) (0, _utils._remove)(listeners[event_name], listener);else form.removeEventListener(event_name, listener, use_capture);
+      return instance;
+    }
+  };
+
+  (0, _utils._defineProperty)(instance, 'data', function () {
+    if (options.processParams) {
+      return options.processParams(formParams(form));
+    }
+    return formParams(form);
+  });
+
+  return instance;
+}
+
+formBind.getParams = formParams;
+
+exports.default = formBind;
