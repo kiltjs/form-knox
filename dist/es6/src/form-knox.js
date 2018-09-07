07aba5ee74e1ae4a8ce9fc08eacfc58635dab744
Jesús Germade <jesus@germade.es>
2018-09-07 23:35:21 +0200

    v1.0.79

---
+++ dist/es6/src/form-knox.js
@@ -0,0 +1,144 @@
+
+import { _noop, _remove, _defineProperty } from './utils';
+
+function _getInputModel (input) {
+  return input.model instanceof Function ? input.model() : input.model;
+}
+
+function _getInputValue (input) {
+  if( 'model' in input ) return _getInputModel(input);
+  return input.value;
+}
+
+var _each = Array.prototype.forEach;
+
+function formParams (form, selector) {
+  if( !(form instanceof HTMLElement) && form.length ) form = form[0];
+  if( !(form instanceof HTMLElement) ) throw new TypeError('parent node should be an HTMLElement');
+
+  var data = {};
+  _each.call(form.querySelectorAll(selector || 'input, select'), function (input) {
+
+    if( !input.name || input.hasAttribute('disabled') || input.type === 'submit' ) return;
+
+    if( input.nodeName === 'SELECT' ) {
+      data[input.name] = input.selectedIndex ? _getInputValue(input.options[input.selectedIndex]) : null;
+    } else if( input.type === 'radio' ) {
+      if( input.checked ) data[input.name] = _getInputValue(input);
+    } else if( input.type === 'radio' ) {
+      if( 'model' in input ) _getInputModel(input);
+      else data[input.name] = input.checked;
+    } else {
+      data[input.name] = _getInputValue(input);
+    }
+
+  });
+
+  return data;
+}
+
+function disableInputs (form, selector) {
+  _each.call(form.querySelectorAll(selector || 'input, select'), function (input) {
+    if( !input.hasAttribute('disabled') ) input.setAttribute('disabled', '');
+  });
+}
+
+function enableInputs (form, selector) {
+  _each.call(form.querySelectorAll(selector || 'input, select'), function (input) {
+    if( input.hasAttribute('disabled') ) input.removeAttribute('disabled', '');
+  });
+}
+
+function readonlyInputs (form, selector) {
+  _each.call(form.querySelectorAll(selector || 'input, select'), function (input) {
+    if( !input.hasAttribute('readonly') ) input.setAttribute('readonly', '');
+  });
+}
+
+function editableInputs (form, selector) {
+  _each.call(form.querySelectorAll(selector || 'input, select'), function (input) {
+    if( input.hasAttribute('readonly') ) input.removeAttribute('readonly', '');
+  });
+}
+
+function runListeners (listeners, args, this_arg) {
+  for( var i = 0, n = listeners.length ; i < n ; i++ ) {
+    listeners[i].apply(this_arg, args);
+  }
+}
+
+function formBind (form, onSubmit, options) {
+  options = options || {};
+
+  if( !(onSubmit instanceof Function) ) {
+    options = onSubmit || options;
+    onSubmit = _noop;
+  }
+
+  if( options.novalidate ) form.setAttribute('novalidate', '');
+
+  var listeners = { invalid: [], reset: [], submitting: [] },
+      use_capture = options.use_capture || options.use_capture === undefined,
+      _onSubmit = function (e) {
+
+        if( form.hasAttribute('novalidate') ) {
+          var valid = form.checkValidity(),
+              input_invalid = form.querySelector(':invalid');
+
+          onSubmit(e, valid);
+          if( e.defaultPrevented ) return;
+
+          if( !valid ) runListeners( listeners.invalid, [], form);
+          runListeners( listeners.submitting, [valid], form);
+
+          if( options.focus_invalid && input_invalid ) input_invalid.focus();
+
+        } else {
+          runListeners( listeners.submitting, [true], form);
+          onSubmit(e, true);
+        }
+
+      };
+
+  form.addEventListener('submit', _onSubmit, use_capture );
+  form.addEventListener('invalid', function _onInvalid () {
+    runListeners( listeners.invalid, [], form);
+    if( !form.hasAttribute('novalidate') ) runListeners( listeners.submitting, [false], form);
+    form.removeEventListener('invalid', _onInvalid, use_capture);
+    setTimeout(function () {
+      form.addEventListener('invalid', _onInvalid, use_capture);
+    });
+  }, use_capture);
+
+  var instance = {
+    form: form,
+    on: function (event_name, listener, use_capture) {
+      if( listeners[event_name] ) listeners[event_name].push(listener);
+      else form.addEventListener(event_name, listener, use_capture);
+      return instance;
+    },
+    off: function (event_name, listener, use_capture) {
+      if( listeners[event_name] ) _remove(listeners[event_name], listener);
+      else form.removeEventListener(event_name, listener, use_capture);
+      return instance;
+    },
+  };
+
+  _defineProperty(instance, 'data', function () {
+    if( options.processParams ) {
+      return options.processParams( formParams(form) );
+    }
+    return formParams(form);
+  });
+
+  return instance;
+}
+
+formBind.getParams = formParams;
+
+formBind.enable = enableInputs;
+formBind.disable = disableInputs;
+formBind.setReadonly = readonlyInputs;
+formBind.setEditable = editableInputs;
+
+export default formBind;
---
+++ dist/form-knox.js
@@ -0,0 +1,81 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _utils = require('./utils');
+
+var _formHelpers = require('./form-helpers');
+
+function runListeners(listeners, args, this_arg) {
+  for (var i = 0, n = listeners.length; i < n; i++) {
+    listeners[i].apply(this_arg, args);
+  }
+}
+
+function formBind(form, onSubmit, options) {
+  options = options || {};
+
+  if (!(onSubmit instanceof Function)) {
+    options = onSubmit || options;
+    onSubmit = _utils._noop;
+  }
+
+  if (options.novalidate) form.setAttribute('novalidate', '');
+
+  var listeners = { invalid: [], reset: [], submitting: [] },
+      use_capture = options.use_capture || options.use_capture === undefined,
+      _onSubmit = function _onSubmit(e) {
+
+    if (form.hasAttribute('novalidate')) {
+      var valid = form.checkValidity(),
+          input_invalid = form.querySelector(':invalid');
+
+      onSubmit(e, valid);
+      if (e.defaultPrevented) return;
+
+      if (!valid) runListeners(listeners.invalid, [], form);
+      runListeners(listeners.submitting, [valid], form);
+
+      if (options.focus_invalid && input_invalid) input_invalid.focus();
+    } else {
+      runListeners(listeners.submitting, [true], form);
+      onSubmit(e, true);
+    }
+  };
+
+  form.addEventListener('submit', _onSubmit, use_capture);
+  form.addEventListener('invalid', function _onInvalid() {
+    runListeners(listeners.invalid, [], form);
+    if (!form.hasAttribute('novalidate')) runListeners(listeners.submitting, [false], form);
+    form.removeEventListener('invalid', _onInvalid, use_capture);
+    setTimeout(function () {
+      form.addEventListener('invalid', _onInvalid, use_capture);
+    });
+  }, use_capture);
+
+  var instance = {
+    form: form,
+    on: function on(event_name, listener, use_capture) {
+      if (listeners[event_name]) listeners[event_name].push(listener);else form.addEventListener(event_name, listener, use_capture);
+      return instance;
+    },
+    off: function off(event_name, listener, use_capture) {
+      if (listeners[event_name]) (0, _utils._remove)(listeners[event_name], listener);else form.removeEventListener(event_name, listener, use_capture);
+      return instance;
+    }
+  };
+
+  (0, _utils._defineProperty)(instance, 'data', function () {
+    if (options.processParams) {
+      return options.processParams((0, _formHelpers.formParams)(form));
+    }
+    return (0, _formHelpers.formParams)(form);
+  });
+
+  return instance;
+}
+
+exports.default = formBind;
+module.exports = exports['default'];
